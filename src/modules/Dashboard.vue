8816ac5d065aec978d5c314d5dd68902dae70ccd
gamboama_sd2082@CEACCS.NET <gamboama_sd2082@CEACCS.NET>
2019-11-12 13:11:49 +0800

    A

--- src/modules/Dashboard.vue
+++ src/modules/Dashboard.vue
@@ -1,41 +1,42 @@
 <template>
   <div>
-    <b-col></b-col>
-    <b-col>
-    <div id="card">
-      <b-card no-body class="overflow-hidden" style="max-width: 540px;">
-        <center>
-          <b-card-img id="image" :src="require('assets/user.png')" class="rounded-0"></b-card-img>
-          <b-row no-gutters>
-            <b-card-body>
-              <h2>Hi {{username}}</h2>
-              <h3>Welcome to EASAKAY</h3>
-            </b-card-body>
-          </b-row>
-        </center>
-      </b-card>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <label class="input-group-text" for="inputGroupSelect01">From</label>
+      </div>
+      <input class="form-control" type="text" v-model="from" />
+      <div class="input-group-append">
+        <button class="btn btn-primary" v-on:click="getPeople">Search</button>
+      </div>
     </div>
-    </b-col>
-    <b-col></b-col>
+    <div>
+  <b-dropdown id="dropdown" text="Passenger" class="m-md-2">
+    <b-dropdown-item>Adult</b-dropdown-item>
+    <b-dropdown-item>Senior Citizen</b-dropdown-item>
+    <b-dropdown-item>Student</b-dropdown-item>
+  </b-dropdown>
+</div>
   </div>
 </template>
 
 <style scoped>
 #card {
-  margin-top: 30px
-
+  margin-top: 30px;
 }
 #image {
   width: 10%;
   height: 90%;
 }
 h2 {
-  font:bold 50px Times New Roman;
+  font: bold 50px Times New Roman;
   color: magenta;
 }
 h3 {
   font: italic bold 40px Georgia, serif;
   color: yellowgreen;
+}
+#input {
+  margin-left: 30%;
 }
 </style>
 
@@ -44,9 +45,9 @@ export default {
   name: "card",
   data() {
     return {
-      username: sessionStorage.getItem("Username"),
+      username: sessionStorage.getItem("Username")
     };
-  },
+  }
 };
 </script>
 
